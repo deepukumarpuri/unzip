c0999882e7cf55715c1b570ad4bc3b4c2304cb5f
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2022-06-30 10:56:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", '48eb04ae416967495ba9930f87d4f4da')
     BOT_TOKEN = os.environ.get("BOT_TOKEN", '1800034142:AAEyfzZJhELEDfXnuCCbdbFo_Zzm42n7Two')
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", '-1001273138715'))
-    DB_CHANNEL = int(os.environ.get("DB_CHANNEL")) or LOGS_CHANNEL
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", '-1001273138715'))
     MONGODB_URL = os.environ.get("MONGODB_URL", 'mongodb://mongo:wWp1Uq4pxGd0L88A3Owm@containers-us-west-65.railway.app:5653')
     BOT_OWNER = int(os.environ.get("BOT_OWNER", '1851062714'))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/DKBOTZ"
